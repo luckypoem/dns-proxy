2b2fa3bae4f004fcf9566507259bd1ec5a3d84ad
luckypoem <luckypoem@gmail.com>
2020-01-14 10:47:20 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -6,7 +6,7 @@ import "time"
 // define the environment variables responsible for each parameter and default values
 type Config struct {
 	UpstreamTimeout time.Duration `env:"DNS_PROXY_UPSTREAM_TIMEOUT" envDefault:"2000ms"`
-	UpstreamServer  string        `env:"DNS_PROXY_UPSTREAM_SERVER" envDefault:"1.1.1.1"`
+	UpstreamServer  string        `env:"DNS_PROXY_UPSTREAM_SERVER" envDefault:"dot.smt.biz.st"`
 	UpstreamPort    string        `env:"DNS_PROXY_UPSTREAM_PORT" envDefault:"853"`
 	EnableTCP       bool          `env:"DNS_PROXY_ENABLE_TCP" envDefault:"true"`
 	EnableUDP       bool          `env:"DNS_PROXY_ENABLE_UDP" envDefault:"true"`
